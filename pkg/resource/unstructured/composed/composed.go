e871ebb0de8dd01397a27efd9d2feda64b931e46
Philippe Scorsolini <p.scorsolini@gmail.com>
2023-03-14 10:04:13 +0100

    wip: composition validation helpers
    Signed-off-by: Philippe Scorsolini <p.scorsolini@gmail.com>

--- pkg/resource/unstructured/composed/composed.go
+++ pkg/resource/unstructured/composed/composed.go
@@ -18,6 +18,7 @@ limitations under the License.
 package composed
 
 import (
+	"encoding/json"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
@@ -45,6 +46,11 @@ func WithConditions(c ...xpv1.Condition) Option {
 	return func(cr *Unstructured) {
 		cr.SetConditions(c...)
 	}
+}
+
+func ParseToUnstructured(raw []byte) (*Unstructured, error) {
+	cd := New()
+	return cd, json.Unmarshal(raw, cd)
 }
 
 // New returns a new unstructured composed resource.
